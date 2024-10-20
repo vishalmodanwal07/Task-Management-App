6b24ee9d08fc4e7131c5b2e2928f3a32fab240a8
vishal modanwal <vk0764394@gmail.com>
2024-10-20 07:10:10 +0530

    correction in feature

--- task-management-app/pages/index.js
+++ task-management-app/pages/index.js
@@ -1,13 +1,13 @@
 
 
-'use client';
+
 import { useState } from "react";
 
 
 export default function Home() {
   const [tasks , setTasks] = useState([]);
   const [taskInput , setTaskInput] = useState({title : '' , description : '' , priority: 'low'});
-  const [editIndex , setEditIndex] =useState(null);
+  const [editId , setEditId] =useState(null);
   const [searchTerm , setSearchTerm ] =useState('');
 
   const handleAddOrEditTask =()=>{
@@ -15,14 +15,14 @@ export default function Home() {
       alert("please fill in all fields");
       return;
     }
-    if(editIndex !==null){
-      const updatedTasks = tasks.map((task , index)=>{
-        index === editIndex ? {...taskInput , completed : task.completed} : task
-      });
+    if(editId !==null){
+      const updatedTasks = tasks.map((task) =>
+        task.id === editId ? { ...taskInput, id: task.id, completed: task.completed } : task
+      );
       setTasks(updatedTasks);
-      setEditIndex(null);
+      setEditId(null);
     }else{
-      setTasks([...tasks , {...taskInput , completed : false}])
+      setTasks([...tasks , {...taskInput ,id: Date.now(), completed : false}])
     }
     setTaskInput({title : '' , description : '' , priority: 'low'})
 
@@ -32,97 +32,107 @@ export default function Home() {
 
    
   }
-  const handleEditTask =(index)=>{
-    setTaskInput(tasks[index]);
-    setEditIndex(index)
+  const handleEditTask =(id)=>{
+    const taskToEdit = tasks.find(task => task.id === id);
+    setTaskInput({ title: taskToEdit.title, description: taskToEdit.description, priority: taskToEdit.priority });
+    setEditId(id);
   }
-  const handleDeleteTask =(index) => {
-    setTasks(tasks.filter((_, i) => i !==index));
+  const handleDeleteTask =(id) => {
+    setTasks(tasks.filter(task =>task.id !==id));
   }
-  const toggleTaskCompletion =(index)=>{
-    setTasks(tasks.map((task , i)=>
-    i===index ? {...task , completed : !task.completed} : task))
+  const toggleTaskCompletion =(id)=>{
+    setTasks(tasks.map((task)=>
+    task.id ===id?{...task , completed : !task.completed}:task))
   }
-  const filteredTasks = tasks.filter(task => task.title.toLowerCase().includes(searchTerm.toLowerCase()))
-  .sort((a,b )=>{
-    const priorityOrder ={high :1 , medium :2 , low :3}
-    return priorityOrder[a.priority]-[b.priority]
-  })
+  const filteredTasks = tasks
+  .filter(task =>   task.title.toLowerCase().includes(searchTerm.toLowerCase()))
+  .sort((a, b) => {
+    const priorityOrder = { high: 1, medium: 2, low: 3 };
+    return priorityOrder[a.priority] - priorityOrder[b.priority]; 
+  });
+
+
   return (
     <>
-     <div className="wrapper">
-     
-        <h1>TASK MANAGEMENT APP</h1>
-        
-        <input
-          type="text"
-          placeholder="Search Tasks By Title"
+    <div className="header">
+      <h1 className="name">TASK MANAGEMENT APP</h1>
+      <input
+          type="search"
+          placeholder="Search Tasks By Title.."
           value={searchTerm}
           onChange={(e)=>setSearchTerm(e.target.value)}
+          className="search"
          />
-        <h3>Your Tasks</h3>
-         <input
+               
+        <h3 className="t-name">Create Your Tasks</h3>
+        <div className="t-title">
+          <h4>Task Title :</h4>
+        <input
          type="text"
          placeholder="Task Title"
+         className="title"
          value={taskInput.title}
          onChange={(e)=>setTaskInput({
           ...taskInput , title : e.target.value
          })}
          />
+         
+        <h4> Task Priority :</h4>
 
+       <select
+        className="priority"
+        value={taskInput.priority}
+        onChange={(e)=>setTaskInput({
+        ...taskInput , priority : e.target.value
+        })}
+      >
+       <option value="low">low</option>
+       <option value="medium">medium</option>
+       <option value="high">high</option>
+      </select>
+     </div>
+ 
+       <h4 className="t-description">Task</h4>
         <input
          type="text"
-         placeholder="Task Description"
+         placeholder="Task Description..."
+         className="description"
          value={taskInput.description}
          onChange={(e)=>setTaskInput({
           ...taskInput , description : e.target.value
          })}
          />
 
-        <h4>Select Task Priority</h4>
-
-        <select
-         value={taskInput.priority}
-         onChange={(e)=>setTaskInput({
-          ...taskInput , priority : e.target.value
-         })}
-        >
-         <option value="low">low</option>
-         <option value="medium">medium</option>
-         <option value="high">high</option>
-        </select>
-
        <button
+       className="edit-button"
        onClick={handleAddOrEditTask}>
-        {editIndex !==null ? "Edit Task": "Add Task"}
+        {editId !==null ? "Edit Task": "Add Task"}
        </button>
-
-       <ul>
-        {filteredTasks.map((task , index)=>(
-          <li key={index}
+      </div>
+        
+     
+    <ol type="number" className="t-list">
+        {filteredTasks.map((task)=>(
+          
+            <li key={task.id}
           className={`${task.completed ? 'completed' :''} ${task.priority}-priority`}>
-          <h3>{task.title}</h3>
-          <p>{task.description}</p>
-          <p>Priority:{task.priority}</p>
-          <p>status:{task.completed? "Completed" : "pending"}</p>
-          <div>
-            <button onClick={()=>{toggleTaskCompletion(index)}}>Toggle status</button>
-            <button onClick={()=>{handleEditTask(index)}}>Edit</button>
-            <button onClick={()=>{handleDeleteTask(index)}}>Delete</button>
+          <h3 className="pris">Title: {task.title}</h3>
+          <p className="pris">Description: {task.description}</p>
+        <div className="pris"> 
+            <p><span style={{color:"blue"}}>Priority:</span>{task.priority}</p>
+            <p><span style={{color:"green"}}>Status:</span>{task.completed? "Completed" : "pending"}</p>
+        </div>
+          <div className="buttons">
+            <button  className="toggle-button" onClick={()=>{toggleTaskCompletion(task.id)}}>Toggle status</button>
+            <button  className="edit-button" onClick={()=>{handleEditTask(task.id)}}>Edit</button>
           </div>
+          <button  className="delete-button" onClick={()=>{handleDeleteTask(task.id)}}>Delete</button>
           </li>
           
+          
+          
         ))}
-       </ul>
-         
-        
-      
-      
-      
-      
-      
-    </div>
-     
+       </ol>
      
     </>
   );
